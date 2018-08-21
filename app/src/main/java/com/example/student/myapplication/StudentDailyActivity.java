a9ef22cf6bc8c5ffb0102b0517338ca579f85334
Leo Salvador <salvador.leonardo444@gmail.com>
2018-08-21 19:04:54 +0800

    ProjectFit Mobile Application Create

---
+++ app/src/main/java/com/example/student/myapplication/StudentDailyActivity.java
@@ -0,0 +1,18 @@
+package com.example.student.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class StudentDailyActivity extends Fragment{
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.stud_dailyact, container, false);
+        return rootView;
+    }
+}
