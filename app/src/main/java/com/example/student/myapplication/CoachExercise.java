488d39e16f87aa065f4f050b31ac1157e3dbab63
Leo Salvador <salvador.leonardo44@gmail.com>
2018-08-27 15:07:00 +0800

    Create Mobile Project Fit (Update StudentAccount)

--- app/src/main/java/com/example/student/myapplication/CoachExercise.java
+++ app/src/main/java/com/example/student/myapplication/CoachExercise.java
@@ -1,15 +1,23 @@
 package com.example.student.myapplication;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.support.design.widget.FloatingActionButton;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,93 +26,202 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
 
 public class CoachExercise extends Fragment{
-    String facility_stringmian;
-    JSONObject facilityObject;
-    JSONArray facilityArray;
-    CoachActivityAdapter contactAdapterf;
-    ListView exerciselist;
-    FloatingActionButton floatingActionButton;
-    TextView counter;
-    private static  int count = 0;
-    @Override
+
+    private ArrayList<CoachListActivity> itemArrayList;
+    private MyAppAdapter myAppAdapter;
+    private ListView listView;
+    private boolean success = false;
+    private Connection conn;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.prof_exercise, container, false);
-        exerciselist = (ListView) rootView.findViewById(R.id.exerciselist);
-        contactAdapterf = new CoachActivityAdapter(getActivity(),R.layout.activity_layout);
-        exerciselist.setAdapter(contactAdapterf);
-        new facilityProcess().execute();
+        listView = (ListView) rootView.findViewById(R.id.exerciselist);
+        itemArrayList = new ArrayList<CoachListActivity>();
+        SyncData orderData = new SyncData();
+        orderData.execute("");
         return rootView;
-
-
     }
 
+    private class SyncData extends AsyncTask<String, String, String>{
 
-    class facilityProcess extends AsyncTask<Void, Void, String> {
-        String facility_URL;
-        String facility_STRING;
+        String msg = "Connection Error";
+        ProgressDialog progress;
 
         @Override
         protected void onPreExecute() {
-            facility_URL = "http://192.168.43.144/projectfit/exercise.php";
+          progress = ProgressDialog.show(getContext(), "Synchronizing","Loading... Please wait!",true);
         }
 
         @Override
-        protected String doInBackground(Void... params) {
-            try {
-                URL url = new URL(facility_URL);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                StringBuilder stringBuilder = new StringBuilder();
-                while ((facility_STRING = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(facility_STRING).append("\n");
-                }
-                return stringBuilder.toString().trim();
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
+        protected void onPostExecute(String msg) {
+            progress.dismiss();
+            Toast.makeText(getContext(),msg +"",Toast.LENGTH_SHORT).show();
+            if(success == false){
             }
-            return null;
-        }
+            else {
+                try {
+                    myAppAdapter = new MyAppAdapter(itemArrayList, getContext());
+                    listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+                    listView.setAdapter(myAppAdapter);
+                }catch (Exception ex){
 
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-        @Override
-        protected void onPostExecute(String result) {
-
-            facility_stringmian = result;
-            try {
-                facilityObject = new JSONObject(facility_stringmian);
-                facilityArray  = new JSONObject(facility_stringmian).getJSONArray("Exercise");
-                String activity_name,activity_decs;
-                count = 0;
-
-                while (count <facilityArray.length())
-                {
-                    JSONObject SO = facilityArray.getJSONObject(count);
-                    activity_name = SO.getString("exercise");
-                    activity_decs = SO.getString("exercise_desc");
-
-                    CoachContactsActivity contactsf = new CoachContactsActivity(activity_name,activity_decs);
-                    contactAdapterf.add(contactsf);
-                    count ++;
                 }
 
-            } catch (JSONException e) {
-                e.printStackTrace();
-
-
             }
+
         }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                conn = connectionclass();
+
+                if (conn == null) {
+                    success = false;
+                } else {
+                    String query = "SELECT * FROM ACTIVITY_INFO";
+                    Statement stmt = conn.createStatement();
+                    ResultSet rs = stmt.executeQuery(query);
+                    if (rs !=null)
+                    {
+                        while (rs.next()){
+
+                            try {
+                                itemArrayList.add(new CoachListActivity(rs.getString("activity"),rs.getString("activity_desc")));
+                            } catch (Exception ex){
+                                ex.printStackTrace();
+                            }
+                        }
+                     //   msg = "Found";
+                        success = true;
+
+                    }
+                    else {
+                       // msg = "No Data Found";
+                        success = false;
+                    }
+
+                }
+            }
+
+            catch(Exception e){
+               // e.printStackTrace();
+               // Writer writer = new StringWriter();
+               // e.printStackTrace(new PrintWriter(writer));
+                //msg = writer.toString();
+                success = false;
+            }
+            return msg;
+
+        }
+    }
+    @SuppressLint("NewApi")
+    public Connection connectionclass(){
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection= null;
+        String ConnectionURL = null;
+
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://softdevs.database.windows.net:1433;DatabaseName=fitnessproject;user=projectfit@softdevs;password=Amper&and1;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+            connection = DriverManager.getConnection(ConnectionURL);
+        } catch (SQLException se)
+        {
+            Log.e("error here 1: ",se.getMessage());
+        }
+        catch (ClassNotFoundException e)
+        {
+            Log.e("error here 2: ",e.getMessage());
+        }
+        catch (Exception e)
+        {
+            Log.e("error here 3: ",e.getMessage());
+        }
+        return connection;
+    }
+
+    public class MyAppAdapter extends BaseAdapter{
+
+
+
+
+        public class ViewHolder{
+           TextView textActivity;
+           TextView textActivityDesc;
+       }
+
+       public List <CoachListActivity> parkingList;
+
+       public Context context;
+       ArrayList<CoachListActivity> arrayList;
+
+       private MyAppAdapter (List<CoachListActivity> apps, Context context){
+
+           this.parkingList = apps;
+           this.context =context;
+           arrayList = new ArrayList<CoachListActivity>();
+           arrayList.addAll(parkingList);
+       }
+
+        @Override
+        public int getCount() {
+            return parkingList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+           View rowView = convertView;
+           ViewHolder viewHolder = null;
+
+           if (rowView == null){
+               LayoutInflater inflater = getLayoutInflater();
+               rowView = inflater.inflate(R.layout.activity_layout,parent,false);
+               viewHolder = new ViewHolder();
+               viewHolder.textActivity = (TextView) rowView.findViewById(R.id.txActivityName);
+               viewHolder.textActivityDesc = (TextView) rowView.findViewById(R.id.txActivityDecs);
+               rowView.setTag(viewHolder);
+
+           }
+           else {
+               viewHolder =(ViewHolder)convertView.getTag();
+
+           }
+
+           viewHolder.textActivity.setText(parkingList.get(position).getName()+"");
+            viewHolder.textActivityDesc.setText(parkingList.get(position).getImg()+"");
+            return rowView;
+        }
+
     }
 }
